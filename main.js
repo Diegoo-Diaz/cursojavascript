73091c60603fe77e6c7f1712e85a71fadabeb46e
Diegoo-Diaz <ddiegoo49@gmail.com>
2024-05-02 11:44:33 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let resultado = 0
+
+function sumar(num1, num2) {
+    resultado = num1 + num2;
+}
+
+
+
+function mostrar (mensaje){
+    alert(mensaje)
+}
+
+
+sumar(100,9)
+
+
+mostrar(resultado)
